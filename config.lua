2f76c92c37f9a9f5d1f6beb19704a748a23e8c12
Fellow25 <92899080+Fellow25@users.noreply.github.com>
2021-10-21 02:23:03 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,50 @@
+
+blanchisseur             = {}
+
+
+
+blanchisseur.jeveuxmarker = true --- true = Oui | false = Non
+
+blanchisseur.jeveuxblips = true --- true = Oui | false = Non
+
+
+
+blanchisseur.pos = {
+        blip = {
+        position = {x = 1144.06, y = -986.02, z = 45.93}
+    },
+        wash = {
+        position = {x = 1136.02, y = -989.53, z = 46.11}
+    },
+	    coffre = {
+		position = {x = 1132.34, y = -995.62, z = 46.11}
+	},
+		garage = {
+		position = {x = 1119.33, y = -984.2, z = 46.29}
+	},
+		spawnvoiture = {
+		position = {x = 1117.76, y = -983.89, z = 45.72, h = 5.4}
+	},
+		boss = {
+		position = {x = 1132.74, y = -991.14, z = 46.11}
+	},
+}
+
+
+blanchisseur.tenue = {
+	            male = {
+                ['tshirt_1'] = 15, ['tshirt_2'] = 0,
+                ['torso_1'] = 86, ['torso_2'] = 0,
+                ['arms'] = 1,
+                ['pants_1'] = 5, ['pants_2'] = 2,
+                ['shoes_1'] = 8, ['shoes_2'] = 2,
+            },
+
+            female = {
+                ['tshirt_1'] = 60,['tshirt_2'] = 0,
+                ['torso_1'] = 65, ['torso_2'] = 7,
+                ['arms'] = 6, ['arms_2'] = 0,
+                ['pants_1'] = 89, ['pants_2'] = 22,
+                ['shoes_1'] = 8, ['shoes_2'] = 11,
+            }
+}
